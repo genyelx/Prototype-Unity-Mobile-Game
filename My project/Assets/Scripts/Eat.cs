27c4fd8af06e13633c11e75c38570dbd0ae62b07
Geniel Henrique <h3nrique42@gmail.com>
2026-03-18 18:18:51 -0300

    Update Eat.cs

--- My project/Assets/Scripts/Eat.cs
+++ My project/Assets/Scripts/Eat.cs
@@ -2,25 +2,16 @@ using UnityEngine;
 
 public class Eat : MonoBehaviour
 {
-    PlayerHealth playerhealth;
-    
-    private void Start()
-    {
-        playerhealth = GetComponent<PlayerHealth>();
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Can'tEat"))
         {
             Destroy(collision.gameObject);
-            playerhealth.health -= 1;
-            playerhealth.points -= 10;
         }
         else
         {
             Destroy(collision.gameObject);
-            playerhealth.points += 10 ;
         }
     }
 }
